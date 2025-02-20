43232a77c25b001d404e45fa99b2c3f14a4861ec
johnmvv <144792461+johnmvv@users.noreply.github.com>
2025-02-20 09:18:50 +0000

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-gray-400 text-center p-4 mt-10">
+      <p>© {new Date().getFullYear()} John Verwey. All rights reserved.</p>
+    </footer>
+  );
+}
